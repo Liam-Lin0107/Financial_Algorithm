4788e228441e34690cfb5bf81bb9290e1267c251
Liam-Lin0107 <geffc1454@gmail.com>
2022-06-06 22:04:57 +0800

    first commit

---
+++ src/MonteCarlo/Statics/RandomSample.h
@@ -0,0 +1,18 @@
+//
+// Created by 林大中 on .
+//
+/*
+ * normalRV()
+ * .........
+ *
+ *
+ */
+#ifndef HW1_RANDOMSAMPLE_H
+#define HW1_RANDOMSAMPLE_H
+#include <random>
+
+double normalRV();
+
+std::vector<double> generateNormalRVs(int n);
+
+#endif //HW1_RANDOMSAMPLE_H
